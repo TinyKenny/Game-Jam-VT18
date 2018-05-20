f211d23b556e68e02342806db3e1874aeb2b1cb3
TinyKenny <Jonathan@rawet.se>
2018-05-20 15:18:17 +0200

    gamecontroller

---
+++ GameJamVT18/Assets/scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController gameControllerInstance;
+
+	// Use this for initialization
+	void Start () {
+        gameControllerInstance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
